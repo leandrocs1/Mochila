9dc5558219c532588db6060dd841ea5a788f0e75
leandro <carmosantosleandro@gmail.com>
2021-10-28 00:51:02 -0300

    atualizando 00:50 28/10

---
+++ JS Programacao imperativa/Aula5/MicroDesafio.js
@@ -0,0 +1,18 @@
+function polToCm(p){
+    return p * 2.540;
+}
+
+let resultado = polToCm(255);
+
+console.log(resultado);
+
+//////////////////////////////////////////////////
+
+function convertToURL(x){
+    
+    return "http://www."+x+".com.br"
+}
+
+let resultadoConvert = convertToURL("funcaojs");
+console.log(resultadoConvert);
+
