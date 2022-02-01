393e30d8aa60a539ca13d81bd3e811dfddf678d7
leandro <carmosantosleandro@gmail.com>
2022-02-01 18:28:57 -0300

    atualizando

---
+++ JS Programacao imperativa/Aula16/Functions_callBack.js
@@ -0,0 +1,31 @@
+// let somar = (a,b)=>{
+//     return a+b;
+// };
+
+// let multiplicar = (a,b)=>{
+//     return a*b;
+// };
+
+
+// function calculadora(a,b,tipo){
+//     console.log(tipo(a,b));
+// }
+
+// calculadora(2,3,multiplicar)
+
+
+/////////////////////////////////////////////////////////////////
+
+// let funcaoCarro = (callBack)=>{
+//     return callBack;
+// };
+
+// function andar(){
+//     return "o carro está andando"
+// }
+
+// function parar(){
+//     return "o carro parou."
+// }
+
+// console.log(funcaoCarro(parar()));
---
+++ JS Programacao imperativa/Aula16/exercicio1.js
@@ -0,0 +1,30 @@
+// // Transforme as seguintes funções em arrow functions:
+
+// let print =(mensagem)=>{
+//     console.log(mensagem);
+// };
+// print("Olá, bom dia!");
+
+// let soma = (n1, n2)=>{
+//     return n1 + n2;;
+// };
+// console.log(soma(10,10));
+
+
+// let array = ["arroz", "feijão", "batata"];
+
+// array.map((i)=>{
+//     console.log(i)
+// });
+
+// function iniciais(nome, sobrenome){
+//     return nome[0] + sobrenome[0];
+// };
+
+// function bomDia(nome, sobrenome, iniciais){
+//     return 'Olá, '+ iniciais(nome, sobrenome);
+// };
+
+// let resultado = bomDia('Leandro', 'Santos', iniciais);
+
+// console.log(resultado);
