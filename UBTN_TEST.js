557934cd2ddea9cfbe165ebd1bb378bbe91e935b
leandrocs1 <carmosantosleandro@gmail.com>
2022-03-08 23:22:59 -0300

    2

--- UBTN_TEST.js
+++ UBTN_TEST.js
@@ -1,4 +1,4 @@
-const senha = "123mudar";
+const senha = "abc123";
 let user = "Leandro";
 const usuario = user + " Well come! Your acess has been released!";
 
@@ -12,4 +12,4 @@ function acessReleased(password){
 
 
 //Digite a senha para acessar:
-acessReleased("123mudar");
+acessReleased("abc123");
