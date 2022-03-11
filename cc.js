33d98f2b1e221e578ab85dab22a3c81562df4695
leandrocs1 <carmosantosleandro@gmail.com>
2022-03-11 22:28:26 +0000

    add

---
+++ cc.js
@@ -0,0 +1,15 @@
+const senha = "testando123";
+let user = "Leandro";
+const usuario = user + " Well come! Your acess has been released!";
+
+function acessReleased(password){
+    if(senha === password){
+        console.log(usuario);
+    }else{
+        console.log("Invalid user!!!!!!")
+    }
+};
+
+
+//Digite a senha para acessar:
+acessReleased("testando123");
