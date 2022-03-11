079cfbce5d4575e5dcc187cfb07178fd6be58b3d
leandrocs1 <carmosantosleandro@gmail.com>
2022-03-11 22:59:46 +0000

    attempt to creat a blocked system

--- UBTN_TEST.js
+++ UBTN_TEST.js
@@ -1,15 +1,32 @@
 const senha = "testando123";
 let user = "Leandro";
 const usuario = user + " Well come! Your acess has been released!";
+let acumulatorPassIncorrect = 1;
+
 
 function acessReleased(password){
     if(senha === password){
         console.log(usuario);
     }else{
         console.log("Invalid user!!!!!!")
+        acumulatorPassIncorrect++;
+    };
+
+    if(acumulatorPassIncorrect > 3){
+        console.log("user blockeded");
     }
+
+
+    console.log(acumulatorPassIncorrect);
+    
 };
 
 
 //Digite a senha para acessar:
-acessReleased("testando123");
+
+
+
+
+acessReleased("testando12");
+
+
