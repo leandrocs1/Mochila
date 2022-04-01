648460af15a637f7d71731defd90a491633c2631
leandro <carmosantosleandro@gmail.com>
2022-04-01 17:58:17 -0300

    atualizando PC novamente o/

--- FrontEnd2/AulaA2A/forInAndforOf.js
+++ FrontEnd2/AulaA2A/forInAndforOf.js
@@ -1,17 +1,17 @@
 // for in ele intera sobre um objeto:
-let object = {
+let object = { //Usar um nome que tenha haver.
     marca: "ford",
     ano: 2022
 };
 
-for(let index in object){
-    console.log(object[index]);
+for(let objectDetail in object){ //Usar um nome que tenha haver.
+    console.log(object[objectDetail]); // UTILIZAR O COLCHETES E ASPAS PARA PROCURAR ESPECIFICO ['marca']
 }
 
 // O for of por sua vez intera sobre array e strings:
 
-let elementInteract = ["Leandro do Carmo Santos", 1989, 32];
+let users = ["Leandro do Carmo Santos", 1989, 32];  //USAR O MESMO NOME É UMA BOA PRÁTICA.(registers)
 
-for(let prop of array){
-    console.log(prop);
+for(let user of users){   //USAR O MESMO NOME É UMA BOA PRÁTICA. (No singular register)
+    console.log(user);
 };
