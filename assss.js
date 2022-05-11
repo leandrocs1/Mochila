6d6333a77a367878abc41a7c87ec3469acb788e1
leandrocs1 <carmosantosleandro@gmail.com>
2022-05-10 21:59:26 -0300

    atualizar

--- assss.js
+++ assss.js
@@ -1,1 +1,20 @@
-//
+function Cadastro(email, password){
+    this.email = email
+    this.password = password
+};
+
+let cadastros = [
+    new Cadastro(
+    "carmosantos@gmail.com", 090522
+    ),
+    new Cadastro(
+    "catarina@gmail.com", 235587
+    )
+
+];
+
+
+    
+
+
+console.log(cadastros[1].email);
